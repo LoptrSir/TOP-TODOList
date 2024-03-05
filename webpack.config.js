c6a4b14d3d2cd4b2230244a3653b52c06e497e49
LoptrSir <91218015+LoptrSir@users.noreply.github.com>
2024-03-05 12:18:41 -0700

    info.txt 3/5 1212

--- webpack.config.js
+++ webpack.config.js
@@ -3,24 +3,24 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-    mode: "production",
+    mode: "development",
     watch: true,
     entry: {
       index: "./src/index.js",
     },
     devtool: "inline-source-map", 
     devServer: {
-      static: "./", //changed from dist as dist files were moved to the root directory
+      static: "./dist", 
     },
     plugins: [
       new HtmlWebpackPlugin({
-        title: "Production",
+        title: "Development",
         template: "./src/index2.html",
       }),
     ],
     output: {
       filename: "[name].bundle.js", 
-      path: path.resolve(__dirname),
+      path: path.resolve(__dirname, "dist"),
       clean: true,
     },
     module: {
